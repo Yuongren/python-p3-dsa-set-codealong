385a9b4f418190325473698e4c31ab930224e5d8
gitYoungren <youngrengitonga@gmail.com>
2024-12-19 12:14:29 +0300

    completed

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,27 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
 
-    pass
+        for value in enumerable:
+         self.dictionary[value] = True
+    
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True # Add a value as a key on the Dictionary
+        return self                   # Return the updated set
+
+        # set =MySet([1,2,3,4])
+        # pass
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+
+    def size(self):
+      return len(self.dictionary)
+    
+
+    def clear(self):
+        self.dictionary.clear()
